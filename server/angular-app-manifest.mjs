88dc49661c94d5607792a2643186682096e915ee
layonel <layonel.millares@gmail.com>
2025-08-10 16:48:00 -0400

    Fix: Corregir base href en todos los archivos index.html para GitHub Pages

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -13,16 +13,16 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-YBFTOMAG.js",
-      "chunk-5IBZ6EYV.js"
+      "chunk-NYZUF7GX.js",
+      "chunk-GO7336BY.js"
     ],
     "route": "/AngularTableConfiguration/login"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-4OOIDREP.js",
-      "chunk-5IBZ6EYV.js"
+      "chunk-JNHOGQ5H.js",
+      "chunk-GO7336BY.js"
     ],
     "route": "/AngularTableConfiguration/dashboard"
   },
@@ -34,10 +34,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2252, hash: '011bd3454881e53a7d7d17a0a20a2a810edccecbde244115b2837c434ffae26c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1984, hash: '3ceac2bf443f55430f6960f30aeecf9cf28d126af87c1ce1ebdb6f616eab652d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 7248, hash: '346d6aa3c77a2cabafa67c3068164fcb4ea60db562bf5d23553de4162902d889', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 7248, hash: '9a6d239661c2909d4f3367b82bfcc80280f36b555cb949fae3b4aa32fb71c3fe', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'styles-6DUK4UUP.css': {size: 1160, hash: 'v6sLbkzAhxE', text: () => import('./assets-chunks/styles-6DUK4UUP_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 3505, hash: 'c02f24b3e10da18b9250b8e4bc116eb2f56f7fb843286b009de77ba353833b22', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1984, hash: '9c3e90f0438b0b2676f5563ce495a783d065873af48c964afcb00b798dcccc86', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 8976, hash: 'ceb899ed1a32ef12e49614a5a48e1ee863d9cc6310431f5c19f3e74ecaae175b', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 8976, hash: '137e1f9a197c9c86f1bb09a376226e490196b1830bcf6aa5c9660f4c42dd298d', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'styles-VVJZJKXC.css': {size: 2479, hash: '9FR3kyJs/SE', text: () => import('./assets-chunks/styles-VVJZJKXC_css.mjs').then(m => m.default)}
   },
 };
